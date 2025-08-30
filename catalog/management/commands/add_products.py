f9a300a8d1d0b02a0f0fe984b59c2745f05f8a24
Anton-Z <rockangel520@mail.ru>
2025-08-30 18:10:04 +0300

    homework_23 compleat

---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Product, Category
+
+class Command(BaseCommand):
+    help = 'Add test products to the database'
+
+    def handle(self, *args, **kwargs):
+        # Удаляем существующие записи
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        category, _ = Category.objects.get_or_create(name='Смартфоны', description='Лучшие смартфоны по доступным ценам')
+
+        phones = [
+            {'name': 'Ipone', 'description': '256 Gb', 'price': 100000, 'category': category},
+            {'name': 'Samsung', 'description': '128 Gb', 'price': 80000, 'category': category},
+        ]
+
+        for phone_data in phones:
+            phone, created = Product.objects.get_or_create(**phone_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Successfully added phone: {phone.name}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Phone already exists: {phone.name}'))
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название категории", help_text="Введите название категории")
+    description = models.TextField(verbose_name="Описание", help_text="Введите описание", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название продукта", help_text="Введите название продукта")
+    description = models.TextField(verbose_name="Описание", help_text="Введите описание", blank=True, null=True)
+    image = models.ImageField(
+        upload_to="catalog/media",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Добавьте изображение товара",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Категория",
+        help_text="Выберите категорию",
+    )
+    price = models.IntegerField(verbose_name="Цена за покупку", help_text="Введите цену", blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category", "price", "created_at", "updated_at"]
+
+    def __str__(self):
+        return self.name
